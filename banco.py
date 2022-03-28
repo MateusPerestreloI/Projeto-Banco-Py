d8ba354363eadf10c2ac02cf052b6cae1d4f3031
Mateus I <mateuspi.0398@gmail.com>
2022-03-28 19:39:11 -0300

    Implementação do banco: parte 1
    Implementação do código de métodos da classe banco: menu, criar_conta, efetuar_saque, efetuar_deposito, efetuar_transferencia, listar_contas, buscar_conta_por_numero

--- banco.py
+++ banco.py
@@ -12,33 +12,148 @@ def main() -> None:
 
 
 def menu() -> None:
-    pass
+    print('=================================================================')
+    print('------------------------------ ATM ------------------------------')
+    print('----------------------------- Banco -----------------------------')
+    print('=================================================================')
+
+    print('Selecione uma opção no menu: ')
+    print('1 - Criar Conta')
+    print('2 - Efetuar Saque')
+    print('3 - Efetuar Depósito')
+    print('4 - Efetuar Transferência')
+    print('5 - Listar Contas')
+    print('6 - Sair do Sistema')
+
+    opcao: int = int(input())
+
+    if opcao == 1:
+        criar_conta()
+    elif opcao == 2:
+        efetuar_saque()
+    elif opcao == 3:
+        efetuar_deposito()
+    elif opcao == 4:
+        efetuar_transferencia()
+    elif opcao == 5:
+        listar_contas()
+    elif opcao == 6:
+        print('Volte Sempre')
+        sleep(2)
+        exit(0)
+    else:
+        print('Opção inválida')
+        sleep(1)
+        menu()
 
 
 def criar_conta() -> None:
-    pass
+    print('Informa os dados do cliente: ')
+
+    nome: str = input('Nome do cliente: ')
+    email: str = input('E-mail do cliente: ')
+    cpf: str = input('CPF do cliente: ')
+    data_nascimento: str = input('Data de Nascimento do cliente: ')
+
+    cliente: Cliente = Cliente(nome=nome, email=email, cpf=cpf, data_nascimento=data_nascimento)
+
+    conta: Conta = Conta(cliente=cliente)
+
+    contas.append(conta)
+
+    print('Conta criada com sucesso!')
+    print('Dados da conta: ')
+    print(conta)
+    sleep(2)
+    menu()
 
 
 def efetuar_saque() -> None:
-    pass
+    if len(contas) > 0:
+        print('====================== Efetuar Saque ======================')
+        numero: int = int(input('Informe o numero da sua conta: '))
+
+        conta: Conta = buscar_conta_por_numero(numero=numero)
+
+        if conta:
+            valor: float = float(input('Informe o valor do saque: '))
+
+            conta.sacar(valor=valor)
+        else:
+            print(f'Não foi encontrada a conta com número: {numero}')
+    else:
+        print('Ainda não existe(m) conta(s) cadastrada(s)')
+        sleep(1)
+        menu()
 
 
 def efetuar_deposito() -> None:
-    pass
+    if len(contas) > 0:
+        print('====================== Efetuar Depósito ======================')
+        numero: int = int(input('Informe o numero da conta em que irá ser depósitado: '))
+
+        conta: Conta = buscar_conta_por_numero(numero=numero)
+
+        if conta:
+            valor: float = float(input('Informe o valor do depósito: '))
+
+            conta.depositar(valor)
+        else:
+            print(f'Não foi encontrada a conta com número: {numero}')
+    else:
+        print('Ainda não existe(m) conta(s) cadastrada(s)')
+        sleep(1)
+        menu()
 
 
 def efetuar_transferencia() -> None:
-    pass
+    if len(contas) > 0:
+        print('====================== Efetuar Transfêrencia ======================')
+        numero_origem: int = int(input('Informe o numero da conta sua conta: '))
+
+        conta_origem: Conta = buscar_conta_por_numero(numero=numero_origem)
+
+        if conta_origem:
+            numero_destino: int = int(input('Informe o numero da conta conta que irá receber a transferência: '))
+
+            conta_destino: Conta = buscar_conta_por_numero(numero=numero_destino)
+            if conta_destino:
+                valor: float = float(input('Informe o valor da transferência: '))
+
+                conta_origem.transferir(destino=numero_destino, valor=valor)
+            else:
+                print(f'Não foi encontrada a conta de destino com número: {numero_origem} ')
+        else:
+            print(f'Não foi encontrada a sua conta com número: {numero_origem} ')
+    else:
+        print('Ainda não existe(m) conta(s) cadastrada(s)')
+        sleep(1)
+        menu()
 
 
 def listar_contas() -> None:
-    pass
+    if len(contas):
+        print('====================== Listar Contas ======================')
+
+        for conta in contas:
+            print(conta)
+            print('-----------------------')
+            sleep(1)
+    else:
+        print('Ainda não existe(m) conta(s) cadastrada(s)')
+        sleep(1)
+        menu()
 
 
-def buscar_conta_por_numero() -> Conta:
-    pass
+def buscar_conta_por_numero(numero: int) -> Conta:
+    c: Conta = None
+
+    if len(contas) > 0:
+        for conta in contas:
+            if conta.numero == numero:
+                c = conta
+    return c
 
 
 if __name__ == '__main__':
     main()
-
